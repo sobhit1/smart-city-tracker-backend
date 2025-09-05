24e319c3a9ba8b1a172cf3bca25b621d4e1118ee
sobhit-raghav <shobit.raghav@solytics-partners.com>
2025-09-06 00:46:52 +0530

    Initial Smart City Tracker backend setup

---
+++ src/main/java/com/project/smart_city_tracker_backend/SmartCityTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.smart_city_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCityTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCityTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/smart_city_tracker_backend/SmartCityTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smart_city_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCityTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
